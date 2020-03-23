5acbd4b527e931beb0561480c5e097b9b6ec77e9
Operations Research Engineering Software+ <alex@oresoftware.com>
2020-03-23 16:43:13 -0700

    squash-me

--- dist/server/on-git-change.js
+++ dist/server/on-git-change.js
@@ -3,9 +3,6 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const get_watchable_dirs_1 = require("../client/get-watchable-dirs");
 const bunion_1 = require("bunion");
 const uuid = require("uuid");
-exports.remoteURLToRepoPath = new Map();
-exports.repoPathToRemoteURL = new Map();
-exports.repoIdToRepoPath = new Map();
 exports.repoIdToRemoteURL = new Map();
 exports.remoteURLToRepoId = new Map();
 exports.getGitRepoIdFromURL = (urls) => {
@@ -30,19 +27,13 @@ exports.onGitChange = (p, userUuid, cb) => {
     if (!repoId) {
         repoId = uuid.v4();
     }
-    exports.repoIdToRepoPath.set(repoId, { pth: p.repo_path });
     for (const u of urls) {
         exports.remoteURLToRepoId.set(u, { id: repoId });
         exports.repoIdToRemoteURL.set(repoId, { url: u });
-        exports.remoteURLToRepoPath.set(u, { pth: p.repo_path });
-        exports.repoPathToRemoteURL.set(p.repo_path, { url: u });
     }
     [
-        { repoPathToRemoteURL: exports.repoPathToRemoteURL },
-        { remoteURLToRepoPath: exports.remoteURLToRepoPath },
         { remoteURLToRepoId: exports.remoteURLToRepoId },
         { repoIdToRemoteURL: exports.repoIdToRemoteURL },
-        { repoIdToRepoPath: exports.repoIdToRepoPath },
     ]
         .forEach(v => {
         console.log(v);
--- src/server/on-git-change.ts
+++ src/server/on-git-change.ts
@@ -4,9 +4,9 @@ import {ChangePayload, GitPayload, ResultCallback} from "../types";
 import {flattenDeep} from "../client/get-watchable-dirs";
 import log from "bunion";
 import * as uuid from 'uuid';
-export const remoteURLToRepoPath = new Map<string, { pth: string }>();
-export const repoPathToRemoteURL = new Map<string, { url: string }>();
-export const repoIdToRepoPath = new Map<string, { pth: string }>();
+// export const remoteURLToRepoPath = new Map<string, { pth: string }>();
+// export const repoPathToRemoteURL = new Map<string, { url: string }>();
+// export const repoIdToRepoPath = new Map<string, { pth: string }>();
 export const repoIdToRemoteURL = new Map<string, { url: string }>();
 // export const repoPathToRepoId = new Map<string, { id: string }>();
 export const remoteURLToRepoId = new Map<string, { id: string }>();
@@ -48,22 +48,21 @@ export const onGitChange = (p: GitPayload, userUuid: string, cb: ResultCallback)
   }
 
   // repoPathToRepoId.set(p.repo_path, {id: repoId});
-  repoIdToRepoPath.set(repoId, {pth: p.repo_path});
+  // repoIdToRepoPath.set(repoId, {pth: p.repo_path});
 
   for (const u of urls) {
      remoteURLToRepoId.set(u,{id: repoId});
      repoIdToRemoteURL.set(repoId, {url: u});
-     remoteURLToRepoPath.set(u, {pth: p.repo_path});
-     repoPathToRemoteURL.set(p.repo_path, {url: u});
+     // remoteURLToRepoPath.set(u, {pth: p.repo_path});
+     // repoPathToRemoteURL.set(p.repo_path, {url: u});
   }
 
   [
-    {repoPathToRemoteURL},
+    // {repoPathToRemoteURL},
     // {repoPathToRepoId},
-    {remoteURLToRepoPath},
     {remoteURLToRepoId},
     {repoIdToRemoteURL},
-    {repoIdToRepoPath},
+    // {repoIdToRepoPath},
   ]
     .forEach(v => {
       console.log(v); //
