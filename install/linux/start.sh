bf82f6b869d90f2f24494f152758d461798cc4b1
Operations Research Engineering Software+ <alex@oresoftware.com>
2020-03-26 00:35:28 -0700

    squash-me

--- install/linux/start.sh
+++ install/linux/start.sh
@@ -1,21 +1,28 @@
 #!/usr/bin/env bash
 
-set -eo pipefail;
+set +e;
 
 (
-    current_node_version="$(node --version 2> /dev/null)"
+    set -eo pipefail;
+    command -v node || echo 'no node installed'
+    command -v nvm || echo 'no nvm installed'
+
+    export cprev_user_uuid="444"
+
+    current_node_version="$(node --version 2> /dev/null || echo '')"
 
     if [[ "$current_node_version" != 'v13.11'* ]]; then
       echo 'Installing nvm in order to install nodejs...'
       (PROFILE=/dev/null curl -o- 'https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh' | bash) &> /dev/null
       exit_code="$?"
+
       if [[ "$exit_code" != '0' ]]; then
          echo 'Could not install node via nvm' > /dev/stderr;
          exit 1;
       fi
 
-      export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
-      [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+      export NVM_DIR="$HOME/.nvm"
+      [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 
        echo 'Installing nodejs v13.11 via nvm...'
        nvm install '13.11'   # &&   nvm use '13.11'
@@ -24,3 +31,7 @@ set -eo pipefail;
 
     node "$HOME/.local/bin/cprev-agent"
 )
+
+exit_code="$?"
+echo "cprev agent is exiting with code: '$exit_code'"
+exit "$exit_code";
